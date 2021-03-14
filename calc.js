62369ab52a3d0b4ecb0d682ef3a9a6bddb80ad10
Christian <christianvillere@gmail.com>
2021-03-13 20:13:54 -0800

    Add buttons to child divs

--- calc.js
+++ calc.js
@@ -23,8 +23,11 @@ const containerDiv = document.body.querySelector(".container");
 function childDivs (){
     for (i = 0; i < 16; i++) {
         let childDiv = document.createElement("div");
+        let childBut = document.createElement("button")
+        childBut.classList.add("childButton")
         childDiv.classList.add("buttDiv");
         containerDiv.appendChild(childDiv);
+        childDiv.appendChild(childBut)
     }
 }
 
