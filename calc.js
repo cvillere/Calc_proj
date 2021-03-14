88c0d86f6a69809092f59764c6a572af88fbdbca
Christian <christianvillere@gmail.com>
2021-03-13 21:09:56 -0800

    Center buttons inside of child divs

--- calc.js
+++ calc.js
@@ -23,9 +23,9 @@ const containerDiv = document.body.querySelector(".container");
 function childDivs (){
     for (i = 0; i < 16; i++) {
         let childDiv = document.createElement("div");
-        let childBut = document.createElement("button")
-        childBut.classList.add("childButton")
+        let childBut = document.createElement("button");
         childDiv.classList.add("buttDiv");
+        childBut.classList.add("childButton")
         containerDiv.appendChild(childDiv);
         childDiv.appendChild(childBut)
     }
